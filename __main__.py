4f20838513bf815942209dd758b1d4c9e071095a
Modar Yaghi <mfyaghi81@gmail.com>
2023-06-12 05:02:03 +0300

    adding some revised models

--- __main__.py
+++ __main__.py
@@ -3,7 +3,9 @@ import passwords as ps
 import time
 import tkinter as tk
 from tkinter import filedialog
+# import xlsx_processing_rev as xp
 import xlsx_processor as xp
+
 
 initial_dir = r"C:\Users\mfyag\OneDrive - SİVİL HUKUK DERNEĞi\Documents\Family Center\Tracking_System_Collective"
 
@@ -40,6 +42,8 @@ def main():
     # Get the json file path
     json_file = os.path.join(script_dir, 'sheets_details.json')
 
+    passwords = ps.passwords
+
     # Copy selected Excel files to 'consolidated_tracking_tools' directory
     for file in files:
         try:
@@ -47,16 +51,16 @@ def main():
             xp.decrypt_and_copy_xlsx_file(file, dir_path)
             print(f"{file} does not require a password.")
         except Exception as e:
-            # print(f"Failed to decrypt {file} without password: {str(e)}")
+            print(f"Failed to decrypt {file} without password: {str(e)}")
             print("--->")
             # If it fails, try to open the file with each password
-            for password in ps.passwords:
+            for password in passwords:
                 try:
                     xp.decrypt_and_copy_xlsx_file(file, dir_path, password)
                     print(f"Password for {file} is {password}.")
                     break  # If the password is correct, go to the next file
                 except Exception as e:
-                    # print(f"Failed to decrypt {file} with password {password}: {str(e)}")
+                    print(f"Failed to decrypt {file} with password {password}: {str(e)}")
                     print("--->")
                     continue  # If the password is incorrect, try the next password
 
@@ -76,3 +80,75 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# initial_dir = r"C:\Users\mfyag\OneDrive - SİVİL HUKUK DERNEĞi\Documents\Family Center\Tracking_System_Collective"
+
+
+# def select_path(prompt_message, select_type="file", default_dir=initial_dir):
+#     print(prompt_message)
+#     root = tk.Tk()
+#     root.withdraw()  # Hide the main window.
+
+#     if select_type == "file":
+#         path = filedialog.askopenfilenames(initialdir=default_dir)  # Open the file dialog.
+#     elif select_type == "dir":
+#         path = filedialog.askdirectory(initialdir=default_dir)  # Open the directory dialog.
+#     else:
+#         raise ValueError("Invalid selection type. Expected 'file' or 'dir'.")
+
+#     return path
+
+
+# def main():
+#     """Main function to execute the script."""
+
+#     # Select input files
+#     files = select_path("Please select the Excel files to process.", select_type="file")
+
+#     # Select output directory
+#     dir_path = select_path("Please select the directory to write the output file to.", select_type="dir")
+#     # Create a subdirectory for the consolidated files.
+#     dir_path = os.path.join(dir_path, 'consolidated')
+
+#     # Get the current script directory
+#     script_dir = os.path.dirname(os.path.realpath(__file__))
+
+#     # Get the json file path
+#     json_file = os.path.join(script_dir, 'sheets_details.json')
+
+#     # Copy selected Excel files to 'consolidated_tracking_tools' directory
+#     for file in files:
+#         try:
+#             # First, try to open the file without a password
+#             xp.decrypt_and_copy_xlsx_file(file, dir_path)
+#             print(f"{file} does not require a password.")
+#         except Exception as e:
+#             # print(f"Failed to decrypt {file} without password: {str(e)}")
+#             print("--->")
+#             # If it fails, try to open the file with each password
+#             for password in ps.passwords:
+#                 try:
+#                     xp.decrypt_and_copy_xlsx_file(file, dir_path, password)
+#                     print(f"Password for {file} is {password}.")
+#                     break  # If the password is correct, go to the next file
+#                 except Exception as e:
+#                     # print(f"Failed to decrypt {file} with password {password}: {str(e)}")
+#                     print("--->")
+#                     continue  # If the password is incorrect, try the next password
+
+#     # Pause for 5 seconds to allow all write operations to complete
+#     time.sleep(5)
+
+#     # Specify columns and sheets to drop
+#     cols_to_drop = ["Unnamed: 0", "System ID"]
+#     sheets_to_drop = ["GZT_Service_Map", "Glossary", "Drop-down"]
+
+#     # Clean Excel files in the 'consolidated_tracking_tools' directory
+#     xp.clean_and_rename_excel_files(dir_path, cols_to_drop, sheets_to_drop, json_file)
+
+#     # Consolidate all the cleaned Excel files into one
+#     xp.merge_excel_files(dir_path, 'TS_psc_All.xlsx')
+
+
+# if __name__ == '__main__':
+#     main()
---
+++ xlsx_processing_rev.py
@@ -0,0 +1,159 @@
+from io import BytesIO
+import json
+import logging
+import msoffcrypto
+import os
+import pandas as pd
+from typing import List
+
+# Define constants
+EXCEL_EXTENSION = '.xlsx'
+
+# Initialize logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def decrypt_and_copy_xlsx_file(file: str, dst_dir: str, password: str = None) -> None:
+    """Decrypts a password-protected Excel file (if a password is provided) and copies it to the destination
+    directory."""
+    os.makedirs(dst_dir, exist_ok=True)
+
+    fname = os.path.basename(file)
+    if fname.endswith(EXCEL_EXTENSION):
+        logger.info('Found excel file: %s', fname)
+
+        # If a password has been provided, decrypt the Excel file
+        if password is not None:
+            with open(file, "rb") as f:
+                crypto = msoffcrypto.OfficeFile(f)
+                crypto.load_key(password=password)
+                decrypted_file = BytesIO()
+                crypto.decrypt(decrypted_file)
+                decrypted_file.seek(0) # Reset the file pointer to the beginning of the file
+        else:
+            with open(file, "rb") as decrypted_file:
+                # Read the (decrypted) Excel file
+                xls = pd.ExcelFile(decrypted_file)
+                with pd.ExcelWriter(os.path.join(dst_dir, fname)) as writer:
+                    for sheet_name in xls.sheet_names:
+                        df = pd.read_excel(xls, sheet_name)
+                        df.to_excel(writer, sheet_name=sheet_name, index=False)
+                logger.info('Copied file to: %s', os.path.join(dst_dir, fname))
+
+def get_initials(filename: str) -> str:
+    """Extracts the initials from a filename."""
+    logger.info('Extracting initials from filename: %s', filename)
+    parts = filename.split('_')
+    if len(parts) >= 3:
+        initials = parts[2].split('-')[0]
+    else:
+        raise ValueError(f"Unexpected filename format: {filename}")
+    logger.info('Extracted initials: %s', initials)
+    return initials
+
+
+def add_service_provider_column(df: pd.DataFrame, provider_initials: str) -> pd.DataFrame:
+    """Adds a service provider initials column to a DataFrame."""
+    logger.info('Adding service provider initials column: %s', provider_initials)
+    df.insert(0, 'sp', provider_initials)
+    logger.info('Added service provider initials column.')
+    return df
+
+def remove_empty_rows(df: pd.DataFrame) -> pd.DataFrame:
+    """Removes rows from a DataFrame where all cells (except in the '#' and 'Service Provider' columns) are NA."""
+    logger.info('Removing empty rows.')
+    df = df.dropna(axis=0, how='all', subset=df.columns[df.columns.isin(['sp', '#']) == False])
+    logger.info('Removed empty rows.')
+    return df
+
+def load_and_clean_sheet(excel_file: str, sheet_name: str, cols_to_drop: List[str]) -> pd.DataFrame:
+    """Loads and cleans a single sheet from an Excel file."""
+    logger.info('Loading and cleaning sheet: %s', sheet_name)
+    df = pd.read_excel(excel_file, sheet_name, header=None)
+    if all(df.iloc[0].str.contains('Unnamed')):
+        df = df.iloc[1:]
+        df = df.iloc[:, 1:]
+    df.columns = df.iloc[0]
+    df = df.iloc[1:]
+    df = df.reset_index(drop=True)
+    df = df.drop(columns=[col for col in cols_to_drop if col in df.columns])
+    logger.info('Loaded and cleaned sheet.')
+    return df
+
+
+def clean_and_rename_excel_files(dir_path: str, cols_to_drop: List[str], sheets_to_drop: List[str], json_file: str) -> None:
+    """ Cleans Excel files in a directory by dropping specified columns and sheets, and renames sheets and columns."""
+
+    logger.info('Cleaning and renaming Excel files in directory: %s', dir_path)
+
+    # Read the JSON file
+    try:
+        with open(json_file) as f:
+            name_mappings = json.load(f)
+    except Exception as e:
+        logger.error('Error reading JSON file %s: %s', json_file, e)
+        return
+
+    for file in os.listdir(dir_path):
+        if file.endswith(EXCEL_EXTENSION):
+            provider_initials = get_initials(file)
+            file_path = os.path.join(dir_path, file)
+            temp_file_path = os.path.join(dir_path, f'temp_{file}')
+            try:
+                with pd.ExcelFile(file_path) as excel_file:
+                    with pd.ExcelWriter(temp_file_path, engine='openpyxl') as writer:
+                        for i, sheet_name in enumerate(excel_file.sheet_names):
+                            if sheet_name not in sheets_to_drop:
+                                df = load_and_clean_sheet(excel_file, sheet_name, cols_to_drop)
+                                df = add_service_provider_column(df, provider_initials)
+                                df = remove_empty_rows(df)
+
+                                # Get the name mapping for this sheet
+                                name_mapping = name_mappings[i]
+
+                                # Rename the columns
+                                df.columns = name_mapping['columns']
+
+                                # Write the DataFrame to the new Excel file
+                                df.to_excel(writer, sheet_name=name_mapping['name'], index=False)
+
+                os.remove(file_path)
+                os.rename(temp_file_path, file_path)
+            except Exception as e:
+                logger.error("Error processing %s: %s", file_path, e)
+    logger.info('Cleaned and renamed Excel files in directory.')
+
+
+def merge_excel_files(dir_path: str, output_file: str) -> None:
+    """ Merge all Excel files in a directory into one Excel file."""
+    logger.info('Merging Excel files in directory: %s', dir_path)
+    data = {}  # A dictionary to hold data for each sheet
+    first_file = True
+    sheet_order = []
+
+    for file in os.listdir(dir_path):
+        logger.info('Processing file: %s', file)
+        if file.endswith(EXCEL_EXTENSION) and file != output_file:  # Exclude the merge file
+            file_path = os.path.join(dir_path, file)
+            try:
+                xls = pd.ExcelFile(file_path, engine='openpyxl')  # specify the engine manually
+                if first_file:  # For the first file, save the order of the sheets
+                    sheet_order = xls.sheet_names  # Make a list of sheet names if it's the first file.
+                    first_file = False
+
+                for sheet_name in xls.sheet_names:
+                    df = pd.read_excel(xls, sheet_name)
+                    if sheet_name in data:
+                        data[sheet_name] = pd.concat([data[sheet_name], df], ignore_index=True)
+                    else:
+                        data[sheet_name] = df
+            except Exception as e:
+                logger.error("Error processing %s: %s", file_path, e)
+        logger.info('Processed file: %s', file)
+
+    # Write data to 'consolidated.xlsx'
+    with pd.ExcelWriter(os.path.join(dir_path, output_file), engine='openpyxl') as writer:
+        for sheet_name in sheet_order:
+            logger.info('Writing sheet: %s', sheet_name)
+            data[sheet_name].to_excel(writer, sheet_name=sheet_name, index=False)
+            logger.info('Wrote sheet: %s', sheet_name)
